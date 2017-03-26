20370f875f905e92d259933eec4450b8a4d5dd39
madhutime <madhutime@gmail.com>
2017-03-26 01:39:17 +0100

    Initial server commit with user login details

---
+++ server/server.js
@@ -0,0 +1,5 @@
+var config = require('./config/config');
+var app = require('./app');
+
+
+app.listen(config.app.port);
---
+++ server/user/userController.js
@@ -0,0 +1,81 @@
+var Config = require('../config/config.js');
+var User = require('./userSchema');
+var jwt = require('jwt-simple');
+
+module.exports.login = function(req, res){
+
+    if(!req.body.email){
+        res.status(400).send('username required');
+        return;
+    }
+    if(!req.body.password){
+        res.status(400).send('password required');
+        return;
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if (err) {
+            res.status(500).send(err);
+            return
+        }
+
+        if (!user) {
+            res.status(401).send('Invalid Credentials');
+            return;
+        }
+        user.comparePassword(req.body.password, function(err, isMatch) {
+            if(!isMatch || err){
+                res.status(401).send('Invalid Credentials');
+            } else {
+                res.status(200).json({token: createToken(user)});
+            }
+        });
+    });
+
+};
+
+module.exports.signup = function(req, res){
+    if(!req.body.email){
+        res.status(400).send('email required');
+        return;
+    }
+    if(!req.body.password){
+        res.status(400).send('password required');
+        return;
+    }
+
+    var user = new User();
+
+    user.firstname = req.body.firstname;
+    user.lastname = req.body.lastname;
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save(function(err) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        res.status(201).json({token: createToken(user)});
+    });
+};
+
+module.exports.unregister = function(req, res) {
+    req.user.remove().then(function (user) {
+        res.sendStatus(200);
+    }, function(err){
+        res.status(500).send(err);
+    });
+};
+
+function createToken(user) {
+    var tokenPayload = {
+        user: {
+            _id: user._id,
+            email: user.email
+        }
+
+    };
+    return jwt.encode(tokenPayload,Config.auth.jwtSecret);
+};
